88e25982e5f24ac75daf9603a66830f7b9876693
huo <bj051055@126.com>
2013-12-30 13:34:44 +0800

    eCamera new UI with localfiles

---
+++ src/com/howell/webcam/BigImages.java
@@ -0,0 +1,176 @@
+package com.howell.webcam;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
+
+import com.android.howell.webcam.R;
+
+public class BigImages extends Activity implements OnTouchListener, OnGestureListener{
+	private ImageView iv1,iv2;
+	private ViewFlipper mViewFlipper;
+	private LinearLayout ll;
+	private GestureDetector mGestureDetector;
+	private int position;
+	private ArrayList<String> mList;
+	
+	private int imageId;
+	private Bitmap bm;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.big_images);
+		mGestureDetector = new GestureDetector(this);   
+		mViewFlipper = (ViewFlipper) findViewById(R.id.flipper);
+		ll = (LinearLayout)findViewById(R.id.ll);
+		iv1 = (ImageView)findViewById(R.id.iv1);
+		iv2 = (ImageView)findViewById(R.id.iv2);
+		
+		ll.setOnTouchListener(this);   
+		ll.setFocusable(true);   
+		ll.setClickable(true);   
+		ll.setLongClickable(true);   
+        mGestureDetector.setIsLongpressEnabled(true); 
+        
+        Intent intent = getIntent();
+        position = intent.getIntExtra("position", 0);
+        System.out.println("position:"+position);
+        mList = intent.getStringArrayListExtra("arrayList");
+        
+        bm = BitmapFactory.decodeFile(mList.get(position));
+        iv1.setImageBitmap(bm);
+        
+        imageId = R.id.iv1;
+//        for(Integer i:mList){
+//        	System.out.println(i);
+//        }
+		
+//		Bitmap bitmap = (Bitmap)getIntent().getParcelableExtra("bitmap");
+//		System.out.println(bitmap.toString());
+//		iv = (ImageView)findViewById(R.id.iv);
+//		iv.setImageBitmap(bitmap);
+//		
+//		iv.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View arg0) {
+//				// TODO Auto-generated method stub
+//				OtherActivity.this.finish();
+//				overridePendingTransition(R.anim.zoomin, R.anim.zoomout);  
+//			}
+//		});
+	}
+	@Override
+	public boolean onDown(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		 Log.e("onFling", "Fling");
+		final int FLING_MIN_DISTANCE = 100, FLING_MIN_VELOCITY = 200;   
+		if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE && Math.abs(velocityX) > FLING_MIN_VELOCITY) {   
+	        // Fling left   
+			mViewFlipper.setInAnimation(this, R.anim.push_left_in);
+			mViewFlipper.setOutAnimation(this, R.anim.push_left_out);
+			if(position == mList.size() - 1){
+				//position = -1;
+				return true;
+			}
+			System.out.println("position:"+position);
+			bm = BitmapFactory.decodeFile(mList.get(position + 1));
+	    	if(imageId == R.id.iv1){
+//	    		iv2.setImageDrawable(getResources().getDrawable(mList.get(position + 1)));
+	    		iv2.setImageBitmap(bm);
+		    	mViewFlipper.showNext();
+				imageId = R.id.iv2;
+			}else{
+//				iv1.setImageDrawable(getResources().getDrawable(mList.get(position + 1)));
+				iv1.setImageBitmap(bm);
+				mViewFlipper.showNext();
+				imageId = R.id.iv1;
+			}
+	    	position ++;
+	        Log.e("MyGesture", "Fling left "+"x:"+Math.abs(e1.getX() - e2.getX())+"y:"+Math.abs(e1.getY() - e2.getY()));  
+	    } else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE && Math.abs(velocityX) > FLING_MIN_VELOCITY) {   
+	        // Fling right   
+	    	mViewFlipper.setInAnimation(this, R.anim.push_right_in);
+			mViewFlipper.setOutAnimation(this, R.anim.push_right_out);
+	    	if(position == 0){
+				//position = mList.size() ;
+				return true;
+			}
+	    	System.out.println("position:"+position);
+	    	bm = BitmapFactory.decodeFile(mList.get(position - 1));
+			if(imageId == R.id.iv1){
+//				iv2.setImageDrawable(getResources().getDrawable(mList.get(position - 1)));
+				iv2.setImageBitmap(bm);
+				mViewFlipper.showPrevious();
+				imageId = R.id.iv2;
+			}else{
+//				iv1.setImageDrawable(getResources().getDrawable(mList.get(position - 1)));
+				iv1.setImageBitmap(bm);
+				mViewFlipper.showPrevious();
+				imageId = R.id.iv1;
+			}
+			position--;
+	        Log.e("MyGesture", "Fling right "+"x:"+Math.abs(e1.getX() - e2.getX())+"y:"+Math.abs(e1.getY() - e2.getY()));   
+	    } else if (e2.getY() - e1.getY() > FLING_MIN_DISTANCE && Math.abs(velocityY) > FLING_MIN_VELOCITY) {   
+	        // Fling Down   
+	        Log.e("MyGesture", "Fling Down "+"y:"+Math.abs(e1.getY() - e2.getY())+"x:"+Math.abs(e1.getX() - e2.getX()));   
+	    } else if (e1.getY() - e2.getY() > FLING_MIN_DISTANCE && Math.abs(velocityY) > FLING_MIN_VELOCITY) {   
+	        // Fling Up   
+	        Log.e("MyGesture", "Fling Up "+"y:"+Math.abs(e1.getY() - e2.getY())+"x:"+Math.abs(e1.getX() - e2.getX()));   
+	    } else{
+	        return true;
+	    }
+		return true;
+	}
+	@Override
+	public void onLongPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		Log.e("onLongPress", "onLongPress");
+	}
+	@Override
+	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
+			float arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void onShowPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		Log.e("onShowPress", "onShowPress");
+	}
+	@Override
+	public boolean onSingleTapUp(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		Log.e("onSingleTapUp", "onSingleTapUp");
+		BigImages.this.finish();
+		overridePendingTransition(R.anim.zoomin, R.anim.zoomout);  
+		return true;
+	}
+	@Override
+	public boolean onTouch(View arg0, MotionEvent event) {
+		// TODO Auto-generated method stub
+		Log.e("onTouch", "onTouch");
+		return mGestureDetector.onTouchEvent(event);   
+	}
+
+}
--- src/com/howell/webcam/LocalFilesActivity.java
+++ src/com/howell/webcam/LocalFilesActivity.java
@@ -4,25 +4,36 @@ import java.io.File;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import com.android.howell.webcam.R;
 
 public class LocalFilesActivity extends Activity {
-	private ArrayList<File> mFileList;
-	private TableLayout table;
+	private ListView listview;
+	private ArrayList<String> mList ;
 	private int imageWidth;
 	private int imageHeight;
+	LinearLayout.LayoutParams lp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -30,48 +41,146 @@ public class LocalFilesActivity extends Activity {
 		setContentView(R.layout.local_files);
 		imageWidth = PhoneConfig.getPhoneWidth(getApplicationContext())/3;
 		imageHeight = imageWidth * 3 / 4;
-		mFileList = new ArrayList<File>();
-		table = (TableLayout)findViewById(R.id.table_for_local_files);
 		File f = new File("/sdcard/eCamera");
-		getFile(f);
-System.out.println("pictures:"+mFileList.size());
-		LinearLayout.LayoutParams lp = new TableRow.LayoutParams(imageWidth, imageHeight);
+		lp = new LinearLayout.LayoutParams(imageWidth, imageHeight);
 		lp.setMargins(0, 0, 0, 10);
-		int j ;
-		for(int i = 0 ; i < mFileList.size() ; i++){
-			System.out.println(f.getPath()+"/"+mFileList.get(i).getName());
-			TableRow row = new TableRow(LocalFilesActivity.this);
-			row.setGravity(Gravity.CENTER);
-			for(j = i ; j < i+3 ; j++){
-				if(j == mFileList.size()){
-		        	break;
-		        }
-				ImageView imageView = new ImageView(LocalFilesActivity.this);
-	//			imageView.setLayoutParams(imgvwDimens);
-				
-				BitmapFactory.Options options = new BitmapFactory.Options();
-		        options.inSampleSize = 2;
-		        Bitmap bm = BitmapFactory.decodeFile(f.getPath()+"/"+mFileList.get(j).getName(), options);
-		        // SET SCALETYPE
-	//	        imageView.setScaleType(ScaleType.FIT_XY);
-		        imageView.setImageBitmap(bm);
-		        
-		        imageView.setLayoutParams(lp);
-				row.addView(imageView);
+		listview = (ListView)findViewById(R.id.lv_localfiles);
+		mList = new ArrayList<String>();
+		getFileName(f);
+		MyAdapter adapter = new MyAdapter(this);
+		listview.setAdapter(adapter);
+	}
+	
+	public ArrayList<String> getFileName(File file){
+		File[] fileArray = file.listFiles();
+		for (File f : fileArray) {
+			System.out.println(f.getPath());
+			if(f.isFile()){
+				mList.add(f.getPath());
 			}
-			i = j - 1;
-			table.addView(row);
+		}
+		return mList;
+	}
+	
+	public class MyAdapter extends BaseAdapter{
+		private Context mContext;
+		
+		public MyAdapter(Context mContext) {
+			super();
+			this.mContext = mContext;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			if(mList == null){
+				return 0;
+			}
+			if(mList.size()%3 == 0){
+				return mList.size() / 3;
+			}else{
+				return mList.size() / 3 + 1;
+			}
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			 return mList == null ? null : mList.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			System.out.println(position);
+			int firstImagePostion = position * 2 + position ;
+			int secondImagePositon = position * 2 + position + 1;
+			int thirdImagePositon = position * 2 + position + 2;
+			ViewHolder holder = null;
+            if (convertView == null) {
+            	LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+				convertView = layoutInflater.inflate(R.layout.localfile_item, null);
+				holder = new ViewHolder();
+				
+				holder.iv1 = (ImageView)convertView.findViewById(R.id.imageView1);
+				holder.iv2 = (ImageView)convertView.findViewById(R.id.imageView2);
+				holder.iv3 = (ImageView)convertView.findViewById(R.id.imageView3);
+				
+				holder.iv1.setOnClickListener(listener);
+				holder.iv2.setOnClickListener(listener);
+				holder.iv3.setOnClickListener(listener);
+				
+				convertView.setTag(holder);
+            }else{
+            	holder = (ViewHolder)convertView.getTag();
+            }
+            
+            if(firstImagePostion == mList.size()){
+            	holder.iv1.setVisibility(View.GONE);
+            	holder.iv2.setVisibility(View.GONE);
+	        	holder.iv3.setVisibility(View.GONE);
+            	return convertView;
+            }else{
+            	holder.iv1.setVisibility(View.VISIBLE);
+            	holder.iv2.setVisibility(View.VISIBLE);
+            	holder.iv3.setVisibility(View.VISIBLE);
+            }
+            Bitmap bm = BitmapFactory.decodeFile(mList.get(firstImagePostion));
+            holder.iv1.setImageBitmap(bm);
+            holder.iv1.setLayoutParams(lp);
+            holder.iv1.setTag(firstImagePostion);
+            System.out.println(mList.get(firstImagePostion)+","+firstImagePostion);
+	        if(secondImagePositon == mList.size()){
+	        	holder.iv2.setVisibility(View.GONE);
+	        	holder.iv3.setVisibility(View.GONE);
+            	return convertView;
+            }else{
+            	holder.iv2.setVisibility(View.VISIBLE);
+            	holder.iv3.setVisibility(View.VISIBLE);
+            }
+	        bm = BitmapFactory.decodeFile(mList.get(secondImagePositon));
+	        holder.iv2.setImageBitmap(bm);
+	        holder.iv2.setLayoutParams(lp);
+	        holder.iv2.setTag(secondImagePositon);
+	        System.out.println(mList.get(secondImagePositon)+","+secondImagePositon);
+            if(thirdImagePositon == mList.size()){
+            	holder.iv3.setVisibility(View.GONE);
+            	return convertView;
+            }else{
+            	holder.iv3.setVisibility(View.VISIBLE);
+            }
+            bm = BitmapFactory.decodeFile(mList.get(thirdImagePositon));
+	        holder.iv3.setImageBitmap(bm);
+            holder.iv3.setLayoutParams(lp);
+            holder.iv3.setTag(thirdImagePositon);
+            System.out.println(mList.get(thirdImagePositon)+","+thirdImagePositon);
+			return convertView;
 		}
 	}
 	
-	public ArrayList<File> getFile(File file){
-		File[] fileArray = file.listFiles();
-		for (File f : fileArray) {
-			System.out.println(f.getName());
-			if(f.isFile()){
-				mFileList.add(f);
-			}
-		}
-		return mFileList;
+	public static class ViewHolder {
+		public ImageView iv1,iv2,iv3;
 	}
+	
+	private OnClickListener listener = new OnClickListener() {
+
+		@Override
+		public void onClick(View view) {
+			// TODO Auto-generated method stub
+			System.out.println(view.getTag());
+			//放大缩小跳转  
+			Intent intent = new Intent(LocalFilesActivity.this, BigImages.class);
+			intent.putExtra("position", Integer.valueOf(view.getTag().toString()));
+			intent.putStringArrayListExtra("arrayList", mList);
+	        startActivity(intent);  
+	        overridePendingTransition(R.anim.zoomin, R.anim.zoomout);  
+		}
+		
+	};
 }
